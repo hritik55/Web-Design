c6d6d0330e309ac426b2644f91bf4b85e29355ce
hritik55 <hbhritik@gmail.com>
2020-04-22 18:25:37 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+var content = document.getElementById('info');
+
+
+function changeContentAS(){
+    content.innerHTML = "<h3>Haghia Sophia</h3><p>Hagia Sophia is the former Greek Orthodox Christian patriarchal cathedral, later an Ottoman imperial mosque and now a museum in Istanbul, Turkey. Built in AD 537 during the reign of Justinian. It is famous for its large dome. It was the world's largest building and an engineering marvel of its time.</p>"
+
+}
+
+function changeContentGK(){
+    content.innerHTML = "<h3>Galata Kulesi</h3><p>The Galata Tower (Galata Kulesi in Turkish) — called Christea Turris (the Tower of Christ in Latin) by the Genoese — is a medieval stone tower in the Galata/Karaköy quarter of Istanbul, Turkey, just to the north of the Golden Horn's junction with the Bosphorus. It is a high, cone-capped cylinder that dominates the skyline and offers a panoramic vista of Istanbul's historic peninsula and its environs.</p>"
+}
+
+function changeContentCD(){
+    content.innerHTML = "<h3>Cappadocia</h3><p>Cappadocia, a semi-arid region in central Turkey, is known for its distinctive “fairy chimneys,” tall, cone-shaped rock formations clustered in Monks Valley, Göreme and elsewhere. Other notables sites include Bronze Age homes carved into valley walls by troglodytes (cave dwellers) and later used as refuges by early Christians. The 100m-deep Ihlara Canyon houses numerous rock-face churches.</p>"
+}
+
+
+function changeContentTP(){
+    content.innerHTML = "<h3>Topkapi Palace</h3><p>The Topkapı Palace, or the Seraglio, is a large museum in the east of the Fatih district of Istanbul in Turkey. In the 15th and 16th centuries it served as the main residence and administrative headquarters of the Ottoman sultans.</p>"
+}
+
+function changeContentPK(){
+    content.innerHTML = "<h3>Pamukkale</h3><p>Pamukkale is a town in western Turkey known for the mineral-rich thermal waters flowing down white travertine terraces on a nearby hillside. It neighbors Hierapolis, an ancient Roman spa city founded around 190 B.C. Ruins there include a well-preserved theater and a necropolis with sarcophagi that stretch for 2km. The Antique Pool is famous for its submerged Roman columns, the result of an earthquake.</p>"
+}
+function clearContent(){
+    content.innerHTML = "";
+}
